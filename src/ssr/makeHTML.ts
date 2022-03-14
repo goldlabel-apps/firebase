cdfdf4cdb8af192142c48fb88d3c6fb304a2fe12
milky <listingslab@gmail.com>
2022-03-14 15:03:25 +1000

    tidy import-map

--- src/ssr/makeHTML.ts
+++ src/ssr/makeHTML.ts
@@ -30,7 +30,8 @@ export default function makeHTML(req:any) {
       menuURL, sharedURL;
   
   const siteTitle = "Listingslab Software";
-  const themeColor = "#126970";
+  const themeDark = "#126970";
+  const themeLight = "#CCCCCC";
 
   switch( env ) {
 
@@ -57,25 +58,25 @@ export default function makeHTML(req:any) {
     excerpt: "Server Side Rendering with Serverless Isomorphic Node and stuff like that",
     ogImage: `${assetsURL}png/opengraph.png`,
     assetsURL, siteURL, rootConfigURL, 
-    themeColor, siteTitle, siteIcon,
+    themeDark, themeLight, siteTitle, siteIcon,
     menuURL, sharedURL,
   }; 
   const {title, excerpt, ogImage } = content;
   
-  const html = `<!DOCTYPE html>
+  return `<!DOCTYPE html>
   <html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="${themeColor}" />
+      <meta name="theme-color" content="${themeDark}" />
       <meta itemprop="name" content="${siteTitle}" />
       <meta name="${siteTitle}" />
 
       <title>${title} ${version}</title>
       
       <meta name="description" content="${excerpt}" />
-      <meta name="keywords" content="ssr, node, react, PWA, " />
-    
+      <meta name="keywords" content="ssr, node, react, PWA " />
+  
       <meta name="geo.region" content="Mediterranean Islands" />
       <meta name="geo.placename" content="Island Countries" />
       <meta name="geo.position" content="35.88999533; 14.43971116" />
@@ -87,7 +88,7 @@ export default function makeHTML(req:any) {
       <meta property="og:description" content="${excerpt}" />
       <meta property="og:image" content="${ogImage}" />
 
-      <meta name="twitter:site" content="https://listingslab.com" />
+      <meta name="twitter:site" content="https://listingslab.com/twitter" />
       <meta name="twitter:title" content="${title}" />
       <meta name="twitter:description" content="${excerpt}"/>
       <meta name="twitter:creator" content="listigslab" />
@@ -96,25 +97,21 @@ export default function makeHTML(req:any) {
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="HandheldFriendly" content="true" />
-      <meta name="msapplication-TileColor" content="${themeColor}">
+      <meta name="msapplication-TileColor" content="${themeDark}">
       <meta name="msapplication-TileImage" content="${assetsURL}png/ms-icon-144x144.png">
 
       <meta name="importmap-type" content="systemjs-importmap" />
+      <link rel="manifest" href="${assetsURL}manifest.json">
+      <link rel="icon" href="${assetsURL}favicon.ico" />
+      <link rel="apple-touch-icon" href="${assetsURL}png/logo512.png" />
+      <link type="text/css" href="${assetsURL}css/listingslab.css" rel="stylesheet" />
       <script type="systemjs-importmap">
         {
           "imports": {
-            "single-spa": "https://cdn.jsdelivr.net/npm/single-spa@5.9.0/lib/system/single-spa.min.js"
-          }
-        }
-      </script>
-      <script type="systemjs-importmap">
-        {
-          "imports": {
-
             "@listingslab/root-config": "${rootConfigURL}",
             "@listingslab/menu": "${menuURL}",
             "@listingslab/shared": "${sharedURL}",
-            
+            "single-spa": "https://cdn.jsdelivr.net/npm/single-spa@5.9.0/lib/system/single-spa.min.js",
             "react": "https://cdn.jsdelivr.net/npm/react@16.13.1/umd/react.production.min.js",
             "react-dom": "https://cdn.jsdelivr.net/npm/react-dom@16.13.1/umd/react-dom.production.min.js",
             "@mui/material":"https://unpkg.com/@mui/material@5.4.3/umd/material-ui.production.min.js"
@@ -122,22 +119,15 @@ export default function makeHTML(req:any) {
         }
       </script>
       ${ getServiceWorker(req) }
-      <link rel="manifest" href="${assetsURL}manifest.json">
-      <link rel="icon" href="${assetsURL}favicon.ico" />
-      <link rel="apple-touch-icon" href="${assetsURL}png/logo512.png" />
-      <link type="text/css" href="${assetsURL}css/listingslab.css" rel="stylesheet" />
-
       <script src="https://cdn.jsdelivr.net/npm/regenerator-runtime@0.13.7/runtime.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/import-map-overrides@2.2.0/dist/import-map-overrides.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/systemjs@6.8.3/dist/system.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/systemjs@6.8.3/dist/extras/amd.min.js"></script>  
 
-      <script>
-var ssr = ${JSON.stringify(content, null, 2)};
-      </script>
+      <script>var ssr = ${JSON.stringify(content, null, 2)};</script>
       
     </head>
-    <body class="stage">
+    <body>
       <noscript>You need a LOT of JavaScript 
         <a
           href="https://listingslab.com/pwa"
@@ -150,22 +140,6 @@ var ssr = ${JSON.stringify(content, null, 2)};
       <script>
         System.import('@listingslab/root-config');
       </script>
-
     </body>
-  </html>`;
-
-  return html;
+  </html>`;   
 }
-
-/*
-<div id="ssr">
-        <div class="landing-element">
-          <img
-            alt="${title}" 
-            src="https://listingslab.com/png/logo192Dark.png" />
-        </div>
-        <div class="um">
-          <h1>${title}</h1>
-        </div>
-      </div>
-*/
