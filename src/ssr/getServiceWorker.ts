89594b4aa9a006ff116c0dc7682a1ac525a00313
milky <listingslab@gmail.com>
2022-03-20 13:12:59 +1000

    19.4.5

--- src/ssr/getServiceWorker.ts
+++ src/ssr/getServiceWorker.ts
@@ -2,10 +2,12 @@ import {getEnv} from "./getEnv";
 export const getServiceWorker = (req:unknown): string => {
   const env = getEnv(req);
   if (env === "LOCAL") return "";
+  const silence = true;
+  if (silence) return "";
   return `<script>
               if ('serviceWorker' in navigator) {
                 window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/service-worker.js')
+                  navigator.serviceWorker.unregister('/service-worker.js')
                   .then(function(registration) {
                   }, function(err) {
                   });
