3ea5355c4045a4d6dc7029ae996acdefa0260ef5
milky <listingslab@gmail.com>
2022-03-26 11:28:12 +1000

    register()

--- src/ssr/get/getServiceWorker.ts
+++ src/ssr/get/getServiceWorker.ts
@@ -2,16 +2,14 @@ import {getEnv} from "./getEnv";
 export const getServiceWorker = (req:unknown): string => {
   const env = getEnv(req);
   if (env === "LOCAL") return "";
-  // const silence = true;
-  // if (silence) return "LLserviceWorker";
   return `<script>
-              if ('serviceWorker' in navigator) {
-                window.addEventListener('load', function() {
-                  navigator.serviceWorker.unregister('/service-worker.js')
-                  .then(function(registration) {
-                  }, function(err) {
-                  });
+            if ('serviceWorker' in navigator) {
+              window.addEventListener('load', function() {
+                navigator.serviceWorker.register('/service-worker.js')
+                .then(function(registration) {
+                }, function(err) {
                 });
-              }
-            </script>`;
+              });
+            }
+          </script>`;
 };
