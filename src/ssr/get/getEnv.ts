185779e66bc3c7d4215eb740803f879c8cab9e31
Wei Zang <weizang75@gmail.com>
2022-04-09 07:55:19 +1000

    refactor FROM typescript

--- src/ssr/get/getEnv.ts
+++
@@ -1,7 +0,0 @@
-/* eslint max-len: "off",@typescript-eslint/ban-ts-comment: "off",  @typescript-eslint/explicit-module-boundary-types: "off" */
-// @ts-nocheck
-export const getEnv = (req:any): string => {
-  let env = "PROD";
-  if ( req.hostname === "localhost" ) env = "LOCAL";
-  return env;
-};
